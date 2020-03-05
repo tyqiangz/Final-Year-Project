48e41378edf0d20d3479d63d4c103105d9f75243
tyqiangz <40680129+tyqiangz@users.noreply.github.com>
2020-03-06 00:04:32 +0800

    Update check_dist_sigma.py

--- check_dist_sigma.py
+++ check_dist_sigma.py
@@ -1,19 +1,24 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
+# This program is used to check accuracy of the counter distributions
 
 from FYP import *
 import pandas as pd
 import scipy as sp
 from scipy.stats import hypergeom
 
-
-# In[35]:
-
-
 def plot1Dist(ax, x, S, d, t, sigmaJ, pmf, title):
+    '''
+    plots a graph comparing the simulation distribution of sigma and that of the model
+    
+    input: 
+    ax: axis to be plotted
+    x: domain of the probability distribution of sigma
+    S: syndrome weight
+    d: block weight
+    t: error weight
+    sigmaJ: distribution of sigma
+    pmf: probability distribution of sigma
+    title: desired title for the graph to be plotted
+    '''
     freqTable = np.array(np.unique(sigmaJ, return_counts=True)).T
     simu = ax.plot(freqTable[:,0], freqTable[:,1], label='Simulation')
     dist = pmf * sum(freqTable[:,1])
@@ -38,6 +43,20 @@ def plot1Dist(ax, x, S, d, t, sigmaJ, pmf, title):
     print("Frequency Table\n", freqTable)
 
 def plotAllDist(x, sigmaJ, e, pmf0, pmf1, FLIPS, S, d, t):
+    '''
+    plots both graphs comparing the simulation distribution of sigma and that of the model
+    
+    input: 
+    sigmaJ: distribution of sigma
+    e: error vector
+    pmf0: probability distribution of sigma given e=0
+    pmf1: probability distribution of sigma given e=1
+    FLIPS: number of flips to be made 
+    S: syndrome weight
+    d: block weight
+    t: error weight
+    title: desired title for the graph to be plotted
+    '''
     ONES = [i for i in range(e.size) if e[i] == 1]
     ZEROS = [j for j in range(e.size) if e[j] == 0]
 
@@ -59,6 +78,7 @@ def genDist(e,H):
     e: error vector
     H: parity-check matrix
     Output:
+    x: domain of sigmaJ
     sigmaJ: [sigma_0, sigma_1, ...], where sigma_j = |s \cap h_j|, where h_j is the jth col of H
     pmf0: distribution of sigma as given by paper for e=0
     pmf1: distribution of sigma as given by paper for e=1
@@ -88,6 +108,9 @@ def flip(e, H, samp_method, FLIPS):
     H: parity-check matrix
     samp_method: sampling method
     FLIPS: number of correct flips
+    
+    Output:
+    e: error vector after FLIPS number of flips
     '''
     r, n = H.shape
     w = sum(H[0,:])
@@ -128,10 +151,6 @@ def flip(e, H, samp_method, FLIPS):
 
     return e
 
-
-# In[ ]:
-
-
 ##################################### Setting parameters ###################################
 
 # code parameters
@@ -151,10 +170,6 @@ print("(r,d,t):", (r,d,t))
 
 H = genQCMDPC(n, r, w)
 
-
-# In[ ]:
-
-
 samp_method = 1
 flips = [0,5,10,20,40,70,100]
 
@@ -172,10 +187,6 @@ for FLIPS in flips:
     PIC_NAME = 'figures/r_d_t_S_samp_method_FLIPS_' + str(r) +'_' + str(d) + '_' + str(t-FLIPS) + '_' + str(S) + '_' + str(samp_method) + '_' + str(FLIPS) + '.png'
     plt.savefig(PIC_NAME)
 
-
-# In[ ]:
-
-
 samp_method = 2
 flips = [0,5,10,20,40,70,100]
 
@@ -192,10 +203,6 @@ for FLIPS in flips:
     PIC_NAME = 'figures/r_d_t_S_samp_method_FLIPS_' + str(r) +'_' + str(d) + '_' + str(t-FLIPS) + '_' + str(S) + '_' + str(samp_method) + '_' + str(FLIPS) + '.png'
     plt.savefig(PIC_NAME)
 
-
-# In[ ]:
-
-
 samp_method = 3
 flips = [0,5,10,20,40,70,100]
 
@@ -211,461 +218,3 @@ for FLIPS in flips:
     
     PIC_NAME = 'figures/r_d_t_S_samp_method_FLIPS_' + str(r) +'_' + str(d) + '_' + str(t-FLIPS) + '_' + str(S) + '_' + str(samp_method) + '_' + str(FLIPS) + '.png'
     plt.savefig(PIC_NAME)
-
-
-# In[25]:
-
-
-##################################### Setting parameters ###################################
-
-# code parameters
-r = 1283
-d = 39
-
-#decryption parameters
-t_init = 27
-
-################################## Processing parameters ###################################
-
-n = 2 * r
-w = 2 * d
-
-##################################### Testing functions ####################################
-print("(r,d,t):", (r,d,t))
-
-H = genQCMDPC(n, r, w)
-
-
-# In[37]:
-
-
-samp_method = 3
-POP_SIZE = 10000
-sigmaJ0, sigmaJ1, fail = {}, {}, {}
-flips = [5]
-
-
-for FLIPS in flips:
-    for z in range(POP_SIZE):
-        print("Iteration %d" % z)
-        # generate random error e, length n, weight t
-        e = genRandomVector(n, t_init)
-        e = flip(e, H, samp_method, FLIPS)
-        
-        if (type(e) == int):
-            continue
-        t = sum(e==1)
-        s = convertBinary(np.matmul(H,e))
-        S = sum(s==1)
-        print("S:", S)
-        X_bar = XBar(S,t,n,w)
-        pi1prime, pi0prime = counterDist(S, X_bar, n, w, d, t)
-        
-        if (samp_method == 1 or samp_method == 2):
-            T = threshold(d, pi1prime, pi0prime, n, t)
-        elif (samp_method == 3):
-            T = int(np.floor(d / 2) + 1)
-
-        j = sampling(H, s, T, samp_method)
-        sigmaj = np.matmul(s, H[:,j])
-
-        if (sigmaj < T):
-            if (not(S in fail)):
-                fail[S] = []
-            fail[S] += [sigmaj]
-            continue
-        elif (e[j] == 0):
-            if (not(S in sigmaJ0)):
-                sigmaJ0[S] = []
-            sigmaJ0[S] += [sigmaj]
-        elif (e[j] == 1):
-            if (not(S in sigmaJ1)):
-                sigmaJ1[S] = []
-            sigmaJ1[S] += [sigmaj]
-print()
-
-print("fail[]:\n", fail)
-print("sigma0[]:\n", sigmaJ0)
-print("sigma1[]:\n", sigmaJ1)
-
-########### generate the model transition probabilities ###########
-if (len(fail) == 0):
-    failProb = 0
-else:
-    failProb = fail / len(fail)
-
-p = calcP(T, n, d, t, w, S, pi1prime, pi0prime)
-# failure probability
-PL = pL(d, pi1prime, pi0prime, p, T, t, n)
-q = calcQ(T, n, d, t, w, S, pi1prime, pi0prime)
-
-transProb0, transProb1 = [[k for k in range(T, d+1)],[]], [[k for k in range(T, d+1)],[]]
-
-if (samp_method == 1):
-    for sigma in range(T, d+1):
-        p_sigma_neg, p_sigma_pos = p_sigmas(n, d, t, w, S, pi1prime, pi0prime, sigma)
-        p_sigma_neg_prime, p_sigma_pos_prime = p_sigmas_prime(p_sigma_neg, p_sigma_pos, PL, p)
-        
-        transProb0[1] += [p_sigma_pos_prime]
-        transProb1[1] += [p_sigma_neg_prime]
-elif (samp_method == 2):
-    for sigma in range(T, d+1):
-        q_sigma_neg, q_sigma_pos = q_sigmas(n, d, t, w, S, pi1prime, pi0prime, sigma)
-        q_sigma_neg_prime, q_sigma_pos_prime = q_sigmas_prime(q_sigma_neg, q_sigma_pos, PL, q)
-        
-        transProb0[1] += [q_sigma_pos_prime]
-        transProb1[1] += [q_sigma_neg_prime]
-    
-elif (samp_method == 3):
-    for sigma in range(T, d+1):
-        q_max_plus, q_max_minus = q_maxs(n, d, t, pi1prime, pi0prime, sigma)
-        
-        transProb0[1] += [q_max_plus]
-        transProb1[1] += [q_max_minus]
-    
-######################## print results ##################################
-
-print("Model failure prob: %f" % PL)
-print("Simulation failure prob: %f\n" % failProb)
-
-print("Model trans prob, e=0")
-print(transProb0)
-
-print("Model trans prob, e=1")
-print(transProb1)
-
-print("Total prob:", sum(transProb1[1])+sum(transProb0[1])+PL)
-
-anotherFailProb = 0
-
-for sigma in range(T):
-    q_max_plus, q_max_minus = q_maxs(n, d, t, pi1prime, pi0prime, sigma)
-    anotherFailProb += q_max_plus + q_max_minus
-
-
-# In[ ]:
-
-
-samp_method = 3
-POP_SIZE = 10000
-sigmaJ0, sigmaJ1, fail = {}, {}, {}
-flips = [10]
-
-
-for FLIPS in flips:
-    for z in range(POP_SIZE):
-        print("Iteration %d" % z)
-        # generate random error e, length n, weight t
-        e = genRandomVector(n, t_init)
-        e = flip(e, H, samp_method, FLIPS)
-        
-        if (type(e) == int):
-            continue
-        t = sum(e==1)
-        s = convertBinary(np.matmul(H,e))
-        S = sum(s==1)
-        print("S:", S)
-        X_bar = XBar(S,t,n,w)
-        pi1prime, pi0prime = counterDist(S, X_bar, n, w, d, t)
-        
-        if (samp_method == 1 or samp_method == 2):
-            T = threshold(d, pi1prime, pi0prime, n, t)
-        elif (samp_method == 3):
-            T = int(np.floor(d / 2) + 1)
-
-        j = sampling(H, s, T, samp_method)
-        sigmaj = np.matmul(s, H[:,j])
-
-        if (sigmaj < T):
-            if (not(S in fail)):
-                fail[S] = []
-            fail[S] += [sigmaj]
-            continue
-        elif (e[j] == 0):
-            if (not(S in sigmaJ0)):
-                sigmaJ0[S] = []
-            sigmaJ0[S] += [sigmaj]
-        elif (e[j] == 1):
-            if (not(S in sigmaJ1)):
-                sigmaJ1[S] = []
-            sigmaJ1[S] += [sigmaj]
-print()
-
-print("fail[]:\n", fail)
-print("sigma0[]:\n", sigmaJ0)
-print("sigma1[]:\n", sigmaJ1)
-
-########### generate the model transition probabilities ###########
-if (len(fail) == 0):
-    failProb = 0
-else:
-    failProb = fail / len(fail)
-
-p = calcP(T, n, d, t, w, S, pi1prime, pi0prime)
-# failure probability
-PL = pL(d, pi1prime, pi0prime, p, T, t, n)
-q = calcQ(T, n, d, t, w, S, pi1prime, pi0prime)
-
-transProb0, transProb1 = [[k for k in range(T, d+1)],[]], [[k for k in range(T, d+1)],[]]
-
-if (samp_method == 1):
-    for sigma in range(T, d+1):
-        p_sigma_neg, p_sigma_pos = p_sigmas(n, d, t, w, S, pi1prime, pi0prime, sigma)
-        p_sigma_neg_prime, p_sigma_pos_prime = p_sigmas_prime(p_sigma_neg, p_sigma_pos, PL, p)
-        
-        transProb0[1] += [p_sigma_pos_prime]
-        transProb1[1] += [p_sigma_neg_prime]
-elif (samp_method == 2):
-    for sigma in range(T, d+1):
-        q_sigma_neg, q_sigma_pos = q_sigmas(n, d, t, w, S, pi1prime, pi0prime, sigma)
-        q_sigma_neg_prime, q_sigma_pos_prime = q_sigmas_prime(q_sigma_neg, q_sigma_pos, PL, q)
-        
-        transProb0[1] += [q_sigma_pos_prime]
-        transProb1[1] += [q_sigma_neg_prime]
-    
-elif (samp_method == 3):
-    for sigma in range(T, d+1):
-        q_max_plus, q_max_minus = q_maxs(n, d, t, pi1prime, pi0prime, sigma)
-        
-        transProb0[1] += [q_max_plus]
-        transProb1[1] += [q_max_minus]
-    
-######################## print results ##################################
-
-print("Model failure prob: %f" % PL)
-print("Simulation failure prob: %f\n" % failProb)
-
-print("Model trans prob, e=0")
-print(transProb0)
-
-print("Model trans prob, e=1")
-print(transProb1)
-
-print("Total prob:", sum(transProb1[1])+sum(transProb0[1])+PL)
-
-anotherFailProb = 0
-
-for sigma in range(T):
-    q_max_plus, q_max_minus = q_maxs(n, d, t, pi1prime, pi0prime, sigma)
-    anotherFailProb += q_max_plus + q_max_minus
-
-
-# In[ ]:
-
-
-samp_method = 3
-POP_SIZE = 10000
-sigmaJ0, sigmaJ1, fail = {}, {}, {}
-flips = [15]
-
-
-for FLIPS in flips:
-    for z in range(POP_SIZE):
-        print("Iteration %d" % z)
-        # generate random error e, length n, weight t
-        e = genRandomVector(n, t_init)
-        e = flip(e, H, samp_method, FLIPS)
-        
-        if (type(e) == int):
-            continue
-        t = sum(e==1)
-        s = convertBinary(np.matmul(H,e))
-        S = sum(s==1)
-        print("S:", S)
-        X_bar = XBar(S,t,n,w)
-        pi1prime, pi0prime = counterDist(S, X_bar, n, w, d, t)
-        
-        if (samp_method == 1 or samp_method == 2):
-            T = threshold(d, pi1prime, pi0prime, n, t)
-        elif (samp_method == 3):
-            T = int(np.floor(d / 2) + 1)
-
-        j = sampling(H, s, T, samp_method)
-        sigmaj = np.matmul(s, H[:,j])
-
-        if (sigmaj < T):
-            if (not(S in fail)):
-                fail[S] = []
-            fail[S] += [sigmaj]
-            continue
-        elif (e[j] == 0):
-            if (not(S in sigmaJ0)):
-                sigmaJ0[S] = []
-            sigmaJ0[S] += [sigmaj]
-        elif (e[j] == 1):
-            if (not(S in sigmaJ1)):
-                sigmaJ1[S] = []
-            sigmaJ1[S] += [sigmaj]
-print()
-
-print("fail[]:\n", fail)
-print("sigma0[]:\n", sigmaJ0)
-print("sigma1[]:\n", sigmaJ1)
-
-########### generate the model transition probabilities ###########
-if (len(fail) == 0):
-    failProb = 0
-else:
-    failProb = fail / len(fail)
-
-p = calcP(T, n, d, t, w, S, pi1prime, pi0prime)
-# failure probability
-PL = pL(d, pi1prime, pi0prime, p, T, t, n)
-q = calcQ(T, n, d, t, w, S, pi1prime, pi0prime)
-
-transProb0, transProb1 = [[k for k in range(T, d+1)],[]], [[k for k in range(T, d+1)],[]]
-
-if (samp_method == 1):
-    for sigma in range(T, d+1):
-        p_sigma_neg, p_sigma_pos = p_sigmas(n, d, t, w, S, pi1prime, pi0prime, sigma)
-        p_sigma_neg_prime, p_sigma_pos_prime = p_sigmas_prime(p_sigma_neg, p_sigma_pos, PL, p)
-        
-        transProb0[1] += [p_sigma_pos_prime]
-        transProb1[1] += [p_sigma_neg_prime]
-elif (samp_method == 2):
-    for sigma in range(T, d+1):
-        q_sigma_neg, q_sigma_pos = q_sigmas(n, d, t, w, S, pi1prime, pi0prime, sigma)
-        q_sigma_neg_prime, q_sigma_pos_prime = q_sigmas_prime(q_sigma_neg, q_sigma_pos, PL, q)
-        
-        transProb0[1] += [q_sigma_pos_prime]
-        transProb1[1] += [q_sigma_neg_prime]
-    
-elif (samp_method == 3):
-    for sigma in range(T, d+1):
-        q_max_plus, q_max_minus = q_maxs(n, d, t, pi1prime, pi0prime, sigma)
-        
-        transProb0[1] += [q_max_plus]
-        transProb1[1] += [q_max_minus]
-    
-######################## print results ##################################
-
-print("Model failure prob: %f" % PL)
-print("Simulation failure prob: %f\n" % failProb)
-
-print("Model trans prob, e=0")
-print(transProb0)
-
-print("Model trans prob, e=1")
-print(transProb1)
-
-print("Total prob:", sum(transProb1[1])+sum(transProb0[1])+PL)
-
-anotherFailProb = 0
-
-for sigma in range(T):
-    q_max_plus, q_max_minus = q_maxs(n, d, t, pi1prime, pi0prime, sigma)
-    anotherFailProb += q_max_plus + q_max_minus
-
-
-# In[ ]:
-
-
-samp_method = 3
-POP_SIZE = 10000
-sigmaJ0, sigmaJ1, fail = {}, {}, {}
-flips = [20]
-
-
-for FLIPS in flips:
-    for z in range(POP_SIZE):
-        print("Iteration %d" % z)
-        # generate random error e, length n, weight t
-        e = genRandomVector(n, t_init)
-        e = flip(e, H, samp_method, FLIPS)
-        
-        if (type(e) == int):
-            continue
-        t = sum(e==1)
-        s = convertBinary(np.matmul(H,e))
-        S = sum(s==1)
-        print("S:", S)
-        X_bar = XBar(S,t,n,w)
-        pi1prime, pi0prime = counterDist(S, X_bar, n, w, d, t)
-        
-        if (samp_method == 1 or samp_method == 2):
-            T = threshold(d, pi1prime, pi0prime, n, t)
-        elif (samp_method == 3):
-            T = int(np.floor(d / 2) + 1)
-
-        j = sampling(H, s, T, samp_method)
-        sigmaj = np.matmul(s, H[:,j])
-
-        if (sigmaj < T):
-            if (not(S in fail)):
-                fail[S] = []
-            fail[S] += [sigmaj]
-            continue
-        elif (e[j] == 0):
-            if (not(S in sigmaJ0)):
-                sigmaJ0[S] = []
-            sigmaJ0[S] += [sigmaj]
-        elif (e[j] == 1):
-            if (not(S in sigmaJ1)):
-                sigmaJ1[S] = []
-            sigmaJ1[S] += [sigmaj]
-print()
-
-print("fail[]:\n", fail)
-print("sigma0[]:\n", sigmaJ0)
-print("sigma1[]:\n", sigmaJ1)
-
-########### generate the model transition probabilities ###########
-if (len(fail) == 0):
-    failProb = 0
-else:
-    failProb = fail / len(fail)
-
-p = calcP(T, n, d, t, w, S, pi1prime, pi0prime)
-# failure probability
-PL = pL(d, pi1prime, pi0prime, p, T, t, n)
-q = calcQ(T, n, d, t, w, S, pi1prime, pi0prime)
-
-transProb0, transProb1 = [[k for k in range(T, d+1)],[]], [[k for k in range(T, d+1)],[]]
-
-if (samp_method == 1):
-    for sigma in range(T, d+1):
-        p_sigma_neg, p_sigma_pos = p_sigmas(n, d, t, w, S, pi1prime, pi0prime, sigma)
-        p_sigma_neg_prime, p_sigma_pos_prime = p_sigmas_prime(p_sigma_neg, p_sigma_pos, PL, p)
-        
-        transProb0[1] += [p_sigma_pos_prime]
-        transProb1[1] += [p_sigma_neg_prime]
-elif (samp_method == 2):
-    for sigma in range(T, d+1):
-        q_sigma_neg, q_sigma_pos = q_sigmas(n, d, t, w, S, pi1prime, pi0prime, sigma)
-        q_sigma_neg_prime, q_sigma_pos_prime = q_sigmas_prime(q_sigma_neg, q_sigma_pos, PL, q)
-        
-        transProb0[1] += [q_sigma_pos_prime]
-        transProb1[1] += [q_sigma_neg_prime]
-    
-elif (samp_method == 3):
-    for sigma in range(T, d+1):
-        q_max_plus, q_max_minus = q_maxs(n, d, t, pi1prime, pi0prime, sigma)
-        
-        transProb0[1] += [q_max_plus]
-        transProb1[1] += [q_max_minus]
-    
-######################## print results ##################################
-
-print("Model failure prob: %f" % PL)
-print("Simulation failure prob: %f\n" % failProb)
-
-print("Model trans prob, e=0")
-print(transProb0)
-
-print("Model trans prob, e=1")
-print(transProb1)
-
-print("Total prob:", sum(transProb1[1])+sum(transProb0[1])+PL)
-
-anotherFailProb = 0
-
-for sigma in range(T):
-    q_max_plus, q_max_minus = q_maxs(n, d, t, pi1prime, pi0prime, sigma)
-    anotherFailProb += q_max_plus + q_max_minus
-
-
-# In[ ]:
-
-
-
-
